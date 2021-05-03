27337a28523eeda3441186bc254be4c38e51a221
sandysetiawan <36066962+sandysetiawan@users.noreply.github.com>
2021-05-03 20:02:52 +0700

    Tampilan home polantas ref #1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,7 @@ use App\Models\Pengguna;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
-
+//
 class HomeController extends Controller
 {
     public function index()
