1f07e394b25085a84f4109ed6a734a4de8835593
sandysetiawan <36066962+sandysetiawan@users.noreply.github.com>
2021-05-03 20:02:05 +0700

    Login untuk polantas ref #1

--- app/Http/Controllers/PolantasController.php
+++ app/Http/Controllers/PolantasController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Polantas;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
+//
 class PolantasController extends Controller
 {
     public function kelolaPolantas()
